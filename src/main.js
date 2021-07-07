f3b7c98a57635f600b04624d74ecbd21425cd5a3
thomas <thomas@olightworld.com>
2021-07-07 14:11:44 +0800

    登录

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import './plugins/element.js'
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+// 导入全局样式
+import './assets/css/global.css'
+// 导入axios
+import axios from 'axios'
+// 配置接口根地址
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 挂在到Vue实例，设置axios挂着在vue的熟悉上，并定$http来挂载axios 后面可通过this.$http就可以调用axios
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,31 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+// 路由懒加载
+const Login = () => import(/* webpackChunkName: "Login_Home_Welcome" */ '../components/Login.vue')
+const Home = () => import(/* webpackChunkName: "Login_Home_Welcome" */ '../components/Home.vue')
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  // 重定向
+  { path: '/', redirect: '/login' },
+  { path: '/login', component: Login },
+  { path: '/home', component: Home }
 ]
 
 const router = new VueRouter({
   routes
+})
+// 挂载路由导航守卫,to表示将要访问的路径，from表示从哪里来，next是下一个要做的操作 next('/login')强制跳转login
+router.beforeEach((to, from, next) => {
+  // to 将要访问的路径 from 代表冲那个路径跳转而来 next是一个函数 表示放行
+  // 访问登录页，放行
+  if (to.path === '/login') return next()
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  // 没有token, 强制跳转到登录页
+  if (!tokenStr) return next('/login')
+  next()
 })
 
 export default router
