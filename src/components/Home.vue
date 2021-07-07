13120f1eab863c5f33e405454b74f976ecb064da
thomas <thomas@olightworld.com>
2021-07-07 17:45:33 +0800

    后台页面布局以及菜单栏布局

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,41 @@
 <template>
-    <div class="">
-            <!-- home组件 -->
+    <el-container>
+        <!-- 头部区域 -->
+        <el-header>
+            <div>
+                <img src="../assets/logo.png">
+                <span>电商后台管理系统</span>
+            </div>
             <el-button type="info" @click="loginout">退出</el-button>
-    </div>
+        </el-header>
+        <!-- 页面主体区域 -->
+        <el-container>
+            <!-- 侧边栏 -->
+            <el-aside width="200px">
+                <!-- 侧边栏菜单栏区域 -->
+                <el-menu background-color="#333744" text-color="#fff" active-text-color="#ffd04b">
+                    <!-- 一级菜单  -->
+                    <el-submenu index="1">
+                         <!-- 一级菜单的模板区域 -->
+                        <template slot="title">
+                            <i class="el-icon-location"></i>
+                            <span>主菜单</span>
+                        </template>
+                         <!-- 二级菜单 -->
+                        <el-menu-item index="1-3">
+                             <!-- 二级菜单的模板区域 -->
+                            <template slot="title">
+                                <i class="el-icon-location"></i>
+                                <span>子菜单</span>
+                            </template>
+                        </el-menu-item>
+                    </el-submenu>
+                </el-menu>
+            </el-aside>
+            <!-- 右侧内容主体 -->
+            <el-main>Main</el-main>
+        </el-container>
+    </el-container>
 </template>
 <script>
 export default {
@@ -17,5 +50,32 @@ export default {
 }
 </script>
 <style lang="less" scoped>
+.el-container{
+    height:100%;
 
+}
+.el-header{
+    background-color:#373f41;
+    display:flex;
+    justify-content:space-between;
+    padding-left:0;
+    align-items: center;
+    color:#fff;
+    > div{
+        display:flex;
+        align-items:center;
+        img{
+             height: 40px;
+        }
+        span{
+            margin-left: 15px;
+        }
+    }
+}
+.el-aside{
+    background-color: #333744;
+}
+.el-main{
+    background-color: #eaedf1;
+}
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,9 +1,16 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main, Menu, Submenu, MenuItem } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
 // 将Message挂载在vue的属性对象上，并自定义$message变量来承载Message,后面直接this.$message就可以调用Message对象
 Vue.prototype.$message = Message
