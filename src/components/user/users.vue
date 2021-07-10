36f261ae217b0a1d39a898cbf762f72d97556620
limeng92 <limeng19920701@163.com>
2021-07-10 16:59:43 +0800

    用户列表面包屑+搜索栏

--- src/components/user/users.vue
+++ src/components/user/users.vue
@@ -1,7 +1,32 @@
 <template>
     <div>
-        用户列表
-    </div>
+        <!-- 面包屑区域 -->
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+            <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+            <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+        </el-breadcrumb>
+        <!-- 卡片视图 -->
+        <el-card>
+            <!-- layout 布局 row 代表一行，col 代表一列
+                span   属性就是占据多大的位置，一个row总共有24个占位
+                gutter 是element中layout布局中属性，用来控制每一个col的之间间距
+            -->
+            <el-row :gutter="20">
+                <!-- 搜索 添加 -->
+                <el-col :span="8">
+                    <el-input placeholder="请输入内容">
+                        <el-button slot="append" icon="el-icon-search"></el-button>
+                    </el-input>
+                </el-col>
+                <!-- 添加按钮区域 -->
+               <el-col :span="4">
+                   <el-button type="primary">添加</el-button>
+                </el-col>
+            </el-row>
+
+        </el-card>
+  </div>
 </template>
 <script>
 export default {
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,23 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main, Menu, Submenu, MenuItem } from 'element-ui'
+import {
+  Button,
+  Form,
+  FormItem,
+  Input,
+  Message,
+  Container,
+  Header,
+  Aside,
+  Main,
+  Menu,
+  Submenu,
+  MenuItem,
+  Breadcrumb,
+  BreadcrumbItem,
+  Card,
+  Row,
+  Col
+} from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
@@ -12,5 +30,10 @@ Vue.use(Main)
 Vue.use(Menu)
 Vue.use(Submenu)
 Vue.use(MenuItem)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(Col)
 // 将Message挂载在vue的属性对象上，并自定义$message变量来承载Message,后面直接this.$message就可以调用Message对象
 Vue.prototype.$message = Message
